1d00acb55ffe5e0fab6d124781485c4aafd19d7e
joserobjr <jose.rob.jr@gmail.com>
2020-03-29 23:20:06 -0300

    Creates the root project

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "koterite"
+
+includeBuild("koterite-filesystem")
+includeBuild("koterite-annotations")
+includeBuild("koterite-nbt")
+includeBuild("koterite-nbt-io")
+includeBuild("koterite-server")
+
